d29c89559e1928436a80ceb51ed0ec815380a1d3
Cubiwan <cubiwan@gmail.com>
2018-11-29 04:49:30 +0100

    Update jsESLetters.js

--- jsESLetters/jsESLetters.js
+++ jsESLetters/jsESLetters.js
@@ -1,24 +1,24 @@
-   function jsESLetters(){
+function jsESLetters(){
     
-     this.capitalLetters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZAÜÁÉÍÓÚ';
-     this.smallLetters = 'abcdefghijklmnñopqrstuvwxyzüáéíóú';
-     this.numbers = '0123456789';
-     this.simbolsMath = '-+*/%()^';
-     this.simbolsMoney = '-€$.,';
-     this.simbolsEmail = '@.';
-     this.letters = letterMayus+letterMinus;
-     this.numbersMath = numbers+simbolsMath;
-     this.numbersMoney = numbers+simbolsMoney;
-     this.email = simbolsEmail+letters+numbers;
-     this.space = ' ';
-     this.spaceExt = " _-";
-     this.simbolsDate = "-/";
-     this.simbolsTime = ":";
-     this.simbolsDateTime = simbolsDate+simbolsTime;
-    
-      this.cleanLetters = function(text){
+    this.capitalLetters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZAÜÁÉÍÓÚ';
+    this.smallLetters = 'abcdefghijklmnñopqrstuvwxyzüáéíóú';
+    this.numbers = '0123456789';
+    this.simbolsMath = '-+*/%()^';
+    this.simbolsMoney = '-€$.,';
+    this.simbolsEmail = '@.';
+    this.letters = this.capitalLetters+this.smallLetters;
+    this.numbersMath = this.numbers+this.simbolsMath;
+    this.numbersMoney = this.numbers+this.simbolsMoney;
+    this.email = this.simbolsEmail+this.letters+this.numbers;
+    this.space = ' ';
+    this.spaceExt = " _-";
+    this.simbolsDate = "-/";
+    this.simbolsTime = ":";
+    this.simbolsDateTime = this.simbolsDate+this.simbolsTime;
+   
+    this.cleanLetters = function(text){
         return text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-      }
-    
-    
     }
+   
+   
+}
