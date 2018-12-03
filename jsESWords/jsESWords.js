285ee4548bd97fef17af07de39a3d86b34aa69c5
Cubiwan <cubiwan@gmail.com>
2018-12-03 03:50:05 +0100

    Create jsESWords.js

---
+++ jsESWords/jsESWords.js
@@ -0,0 +1,42 @@
+function jsESWords(letters){
+    letters = letters || "[a-zA-ZñÑüáéíóúÜÁÉÍÓÚ1234567890]+";
+    var regExp = RegExp(letters, "g");
+    
+    this.length = function(text){
+        return text.trimEnd().length;
+    }
+
+    this.words = function(text){        
+        var words = [];
+        while ((word = regExp.exec(text)) !== null) {
+            words.push(word[0])
+        }
+        return words;
+    }
+
+    this.sort = function(words){
+        return words.sort(function(a,b){
+            return a.localeCompare(b, "es");
+        })        
+    }
+
+    this.unique = function(words){
+        var uniqueWord = {};
+        for(var i = 0; i < words.length; i++){
+            uniqueWord[words[i]] = words[i];            
+        }
+        return Object.values(uniqueWord);
+    }
+
+    this.histogram = function(words){
+        var histogramWords = {};
+        for(var i = 0; i < words.length; i++){
+            if(histogramWords[words[i]]){
+                histogramWords[words[i]] += 1;
+            } else {
+                histogramWords[words[i]] = 1;
+            }
+        }
+        return histogramWords;
+    }
+}
