f4de213c98e3b8e29966dfae07ec17a2f6f279eb
Cubiwan <cubiwan@gmail.com>
2019-01-15 19:23:26 +0100

    Update jsESreplace.js

--- jsESreplace/jsESreplace.js
+++ jsESreplace/jsESreplace.js
@@ -1,29 +1,17 @@
-function jsESreplace() {
+export function jsESreplace() {
     var replaces = [];
     var defaultReplace;
     
     this.add = function(letter, replace){
         var length = letter.length;
         for(var i = 0; i < length; ++i){
+            console.log(letter[i]+"->"+replace);
             replaces[letter[i]] = replace;
         }
     }
     
-     this.setDefault = function(replace){
+    this.setDefault = function(replace){
         defaultReplace = replace;
-    }
-
-    function replaceLetter(letter){
-       var result = replaces[letter];
-       if(result != undefined){
-         return result;
-       }else{
-           if(defaultReplace != undefined){
-               return defaultReplace;
-           } else {
-               return letter;
-           }
-       }
     }
     
     this.addMultiples = function(letter, replace){
@@ -40,9 +28,9 @@ function jsESreplace() {
         var length = str.length;
         var replaceStr = "";
         for (var i = 0; i < length; ++i) {
-            replaceStr += replaceLetter(str.charAt(i));
+            var letter = str.charAt(i);
+            replaceStr += replaces[letter] || defaultReplace || letter;
         }
         return replaceStr;
     }
-
 }
